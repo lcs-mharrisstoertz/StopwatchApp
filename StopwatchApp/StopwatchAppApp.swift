2d63ec5eee5cdf92d213e58e5b4d897f5f36abdc
Morgan <Mharrisstoertz@gmail.com>
2022-11-12 16:49:51 -0500

    Initial Commit

---
+++ StopwatchApp/StopwatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchAppApp.swift
+//  StopwatchApp
+//
+//  Created by Morgan Harris-Stoertz on 2022-11-12.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
